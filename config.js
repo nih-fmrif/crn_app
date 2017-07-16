2c622dbe85229072ed1383917e7ffd4e7e9a12d8
Denes <dsdenes@gmail.com>
2017-07-16 22:06:57 +0200

    feat: Implement Globus auth service
    Signed-off-by: Denes <dsdenes@gmail.com>

--- config.js
+++ config.js
@@ -21,7 +21,12 @@ export default {
 		 * Authentication
 		 */
 		auth: {
-			type: 'google',
+			type: 'globus',
+			globus: {
+        authUri: process.env.CRN_SERVER_URL + '/crn/auth/globus',
+        refreshTokenUri: process.env.CRN_SERVER_URL + '/crn/auth/globus/refresh',
+        scopes: ['urn:globus:auth:scope:transfer.api.globus.org:all', 'openid', 'profile', 'email', 'offline_access']
+			},
 			google: {
 				clientId: process.env.SCITRAN_AUTH_CLIENT_ID,
 				scopes: 'email,openid'
--- src/scripts/services/auth/globus.js
+++ src/scripts/services/auth/globus.js
@@ -1,14 +1,143 @@
-import queryString  from 'query-string';
+import config from '../../../../config';
+import queryString from 'query-string';
+import axios from 'axios';
 
-export default function () {
+export default function (common, logger) {
+
+  const {
+    accessTokenExpired,
+    assertUser,
+    getAuthProp,
+    getStoredAccessToken,
+    getStoredRefreshToken,
+    hasToken,
+    saveAuthProp,
+    saveOauth,
+    signOut,
+  } = common;
+
+  const getAccessToken = getAccessTokenQueue();
+
   return Object.freeze({
-    handleAuthCallback
+    fetchUser,
+    getAccessToken,
+    handleAuthCallback,
+    init,
+    signIn
   });
-}
 
-function handleAuthCallback() {
-  const url = queryString.parse(window.location.search);
-  if (url.globusOauth) {
-    window.localStorage.auth.oauth = url.globusOauth;
+  async function handleAuthCallback() {
+    const url = queryString.parse(window.location.search);
+    if (url.globusOauth) {
+      try {
+        const oAuth = JSON.parse(url.globusOauth);
+        oAuth.expires = Date.now() + oAuth.expires_in;
+        saveOauth(oAuth);
+      } catch(err) {
+        logger.error('Unknown authentication error.', err);
+      }
+      saveAuthProp('signin_finished', true);
+    }
+  }
+
+  async function fetchUser() {
+    const authInstance = getAuthInstance();
+    const globusUser = await popsicle(authInstance.sign({
+      method: 'get',
+      url: config.auth.globus.userInfoUri
+    })).then(res => JSON.parse(res.body));
+
+    if (globusUser) {
+      const {
+        preferred_username,
+        email,
+        name
+      } = globusUser;
+
+      return {
+        preferred_username,
+        email,
+        name
+      };
+
+    } else {
+      return {};
+    }
+  }
+
+  async function init() {
+    return getAccessToken();
+  }
+
+  function getAccessTokenQueue() {
+    let refreshRequest = null;
+    return async () => {
+      if (hasToken()) {
+        if (accessTokenExpired()) {
+          if (refreshRequest !== null) {
+            return refreshRequest;
+          }
+          refreshRequest = refreshToken();
+          await refreshRequest;
+          refreshRequest = null;
+        }
+        return getStoredAccessToken();
+      } else {
+        return null;
+      }
+    };
+  }
+
+  async function refreshToken() {
+    if (hasToken()) {
+      try {
+        const accessToken = getStoredAccessToken();
+        const refreshToken = getStoredRefreshToken();
+        const oAuth = await axios.post(config.auth.globus.refreshTokenUri, {
+          accessToken,
+          refreshToken
+        }).then(res => res.data);
+        oAuth.expires = Date.now() + oAuth.expires_in;
+        saveOauth(oAuth);
+      } catch (err) {
+        logger.error('Refreshing token failed.', err);
+      }
+    } else {
+      return false;
+    }
+  }
+
+  async function signIn() {
+    logger.info('Signin start.');
+    saveAuthProp('signin_finished', false);
+    await authInNewWindow();
+    await refreshToken();
+    try {
+      await assertUser();
+    } catch (err) {
+      logger.error('Cannot verify user.', err);
+      await signOut();
+      throw err;
+    }
+  }
+
+  async function authInNewWindow() {
+    const authWindow = window.open(config.auth.globus.authUri);
+    await waitUntilInitialized();
+    authWindow.close();
+  }
+
+  function waitUntilInitialized() {
+    return new Promise(isInitialized);
+  }
+
+  function isInitialized(resolve, reject) {
+    if (getAuthProp('signin_finished', false) === true) {
+      logger.debug('Signin finished.');
+      resolve();
+    } else {
+      logger.debug('Signin not yet finished.');
+      setTimeout(() => isInitialized(resolve, reject), 500);
+    }
   }
 }
